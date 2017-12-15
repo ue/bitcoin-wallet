b1a9b14e62f6b2420e378f442677ee1932170150
ue <ugurerdal93@gmail.com>
2017-12-15 23:00:21 +0300

    initial commit for ui

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,14 +2,19 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Home.css';
+import Button from 'material-ui/Button';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
         <div className={styles.container} data-tid="container">
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
+          <h2>The Wallet</h2>
+          <Link to="/counter">
+            <Button raised color="primary">
+              Start
+            </Button>
+          </Link>
         </div>
       </div>
     );
