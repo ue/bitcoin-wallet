111d02fa51f914415aae271bdf2e48135ab31504
ue <ugurerdal93@gmail.com>
2017-12-20 01:12:11 +0300

    added drawer menu for left side also content and title side

--- app/components/wallet/walletView.js
+++ app/components/wallet/walletView.js
@@ -1,23 +1,152 @@
-// @flow
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import styles from './_styles.css';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class Wallet extends Component {
+import {
+  AppBar,
+  Drawer,
+  Toolbar,
+  Typography,
+  List,
+  IconButton,
+  Hidden,
+  Divider,
+  withStyles,
+} from 'material-ui';
+
+import MenuIcon from 'material-ui-icons/Menu';
+
+import { menuListItems, underMenuListItems } from './tileData';
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    height: 500,
+    marginTop: theme.spacing.unit * 3,
+    zIndex: 1,
+    overflow: 'hidden',
+  },
+  appFrame: {
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+  },
+  appBar: {
+    position: 'absolute',
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+  },
+  navIconHide: {
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+  drawerHeader: theme.mixins.toolbar,
+  drawerPaper: {
+    width: 250,
+    [theme.breakpoints.up('md')]: {
+      width: drawerWidth,
+      position: 'relative',
+      height: '100%',
+    },
+  },
+  content: {
+    backgroundColor: theme.palette.background.default,
+    width: '100%',
+    padding: theme.spacing.unit * 3,
+    height: 'calc(100% - 56px)',
+    marginTop: 56,
+    [theme.breakpoints.up('sm')]: {
+      height: 'calc(100% - 64px)',
+      marginTop: 64,
+    },
+  },
+});
+
+class Wallet extends React.Component {
+  state = {
+    mobileOpen: false,
+  };
+
+  handleDrawerToggle = () => {
+    this.setState({ mobileOpen: !this.state.mobileOpen });
+  };
+
   render() {
-    return (
+    const { classes, theme } = this.props;
+
+    const drawer = (
       <div>
-        <div className={styles.backButton} data-tid="backButton">
-          <Link to="/">
-            <i className="fa fa-arrow-left fa-3x" />
-          </Link>
-        </div>
-        <div className={`wallet ${styles.wallet}`} data-tid="wallet">
-          sa
+        <div className={classes.drawerHeader} />
+        <Divider />
+        <List>{menuListItems}</List>
+        <Divider />
+        <List>{underMenuListItems}</List>
+        <List>Ugur ERDAL</List>
+      </div>
+    );
+
+    return (
+      <div className={classes.root}>
+        <div className={classes.appFrame}>
+          <AppBar className={classes.appBar}>
+            <Toolbar>
+              <IconButton
+                color="contrast"
+                aria-label="open drawer"
+                onClick={this.handleDrawerToggle}
+                className={classes.navIconHide}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography type="title" color="inherit" noWrap>
+                Title
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Hidden mdUp>
+            <Drawer
+              type="temporary"
+              anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+              open={this.state.mobileOpen}
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+              onClose={this.handleDrawerToggle}
+              ModalProps={{
+                keepMounted: true,
+              }}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+          <Hidden mdDown implementation="css">
+            <Drawer
+              type="permanent"
+              open
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+          <main className={classes.content}>
+            <Typography noWrap>{'Content'}</Typography>
+          </main>
         </div>
       </div>
     );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(Wallet);
