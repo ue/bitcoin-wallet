0a3c96b12f327a4def9acda595f6b8f97ebaefee
ue <ugurerdal93@gmail.com>
2017-12-23 01:03:08 +0300

    added rooting location for each list items

--- app/components/list/listView.js
+++ app/components/list/listView.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
 import {
   Home,
@@ -13,58 +15,80 @@ import {
 
 export const menuListItems = (
   <div>
-    <ListItem button>
-      <ListItemIcon>
-        <Home />
-      </ListItemIcon>
-      <ListItemText primary="Inbox" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <AccountBalanceWallet />
-      </ListItemIcon>
-      <ListItemText primary="Wallets" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <CompareArrows />
-      </ListItemIcon>
-      <ListItemText primary="Exchange" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <History />
-      </ListItemIcon>
-      <ListItemText primary="History" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <Settings />
-      </ListItemIcon>
-      <ListItemText primary="Settings" />
-    </ListItem>
+    <Link to="/wallet/inbox">
+      <ListItem button>
+        <ListItemIcon>
+          <Home />
+        </ListItemIcon>
+        <ListItemText primary="Inbox" />
+      </ListItem>
+    </Link>
+
+    <Link to="/wallet/wallets">
+      <ListItem button>
+        <ListItemIcon>
+          <AccountBalanceWallet />
+        </ListItemIcon>
+        <ListItemText primary="Wallets" />
+      </ListItem>
+    </Link>
+
+    <Link to="/wallet/exchange">
+      <ListItem button>
+        <ListItemIcon>
+          <CompareArrows />
+        </ListItemIcon>
+        <ListItemText primary="Exchange" />
+      </ListItem>
+    </Link>
+
+    <Link to="/wallet/history">
+      <ListItem button>
+        <ListItemIcon>
+          <History />
+        </ListItemIcon>
+        <ListItemText primary="History" />
+      </ListItem>
+    </Link>
+
+    <Link to="/wallet/settings">
+      <ListItem button>
+        <ListItemIcon>
+          <Settings />
+        </ListItemIcon>
+        <ListItemText primary="Settings" />
+      </ListItem>
+    </Link>
   </div>
 );
 
 export const underMenuListItems = (
   <div>
-    <ListItem button>
-      <ListItemIcon>
-        <InfoOutline />
-      </ListItemIcon>
-      <ListItemText primary="Support" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <CardGiftcard />
-      </ListItemIcon>
-      <ListItemText primary="Invite" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <ExitToApp />
-      </ListItemIcon>
-      <ListItemText primary="Log Out" />
-    </ListItem>
+    <Link to="/wallet/support">
+      <ListItem button>
+        <ListItemIcon>
+          <InfoOutline />
+        </ListItemIcon>
+        <ListItemText primary="Support" />
+      </ListItem>
+    </Link>
+
+    <Link to="/wallet/invite">
+      <ListItem button>
+        <ListItemIcon>
+          <CardGiftcard />
+        </ListItemIcon>
+        <ListItemText primary="Invite" />
+      </ListItem>
+    </Link>
+
+    <Link to="/wallet/logout">
+      <ListItem button>
+        <ListItemIcon>
+          <ExitToApp />
+        </ListItemIcon>
+        <ListItemText primary="Log Out" />
+      </ListItem>
+    </Link>
   </div>
 );
